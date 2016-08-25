92efaddaca607b0f9b168d653248d6723f851abe
jdbuysse <jdbuysse@gmail.com>
2016-08-25 14:38:55 -0400

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+This is a temporary script file.
+"""
+import numpy
+
+
+def square(number):
+    return number*number
+    
+def division_float(number1,number2):
+    return number1/number2
+    
+def first_from_list(list):
+    return list[0]
+
+def pi():
+    return numpy.pi
